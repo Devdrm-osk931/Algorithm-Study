e1cd1134d1c2bec12cbdab1d3a70e465d464a075
Seok Kyo Oh <osk931@naver.com>
2022-05-25 03:40:00 +0900

    Hash#1

---
+++ Programmers/고득점KIT/Hash/완주하지못한선수.py
@@ -0,0 +1,24 @@
+# Programmers_완주하지 못한 선수_Level 1
+
+def solution(participant, completion):
+    answer = ''
+    
+    done = {}
+
+    for c_person in completion:
+        # add people who completed to done dictionary
+        if c_person in done:
+            done[c_person] += 1
+        else:
+            done[c_person] = 1
+    
+    for person in participant:
+        if person in done:
+            done[person] -= 1
+            if not done[person]:
+                del(done[person])
+        else:
+            answer = person
+                
+                
+    return answer
