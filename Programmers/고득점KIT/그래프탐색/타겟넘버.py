005bae77ba0659dc6c1818e5c474bf68b11c4fa7
Seok Kyo Oh <osk931@naver.com>
2022-07-08 12:46:01 +0900

    타겟넘버

---
+++ Programmers/고득점KIT/그래프탐색/타겟넘버.py
@@ -0,0 +1,28 @@
+answer = 0
+
+def make_comb(numbers, comb, target):
+    global answer
+    if len(comb) == len(numbers):
+        case = 0
+        for operation, number in zip(comb, numbers):
+            if operation == '+':
+                case += number
+            else:
+                case -= number
+        if case == target:
+            answer += 1
+        return
+
+    for operation in ['+', '-']:
+        comb.append(operation)
+        make_comb(numbers, comb, target)
+        comb.pop()
+
+
+def solution(numbers, target):
+    make_comb(numbers, [], target)
+
+    return answer
+
+
+print(solution([4, 1, 2, 1], 4))
