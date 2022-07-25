69a8823def1de9858a15ad1cd1999d975ead52d2
Seok Kyo Oh <osk931@naver.com>
2022-07-25 13:02:21 +0900

    피로도

---
+++ Programmers/고득점KIT/완전탐색/피로도.py
@@ -0,0 +1,30 @@
+# Programmers
+def solution(k, dungeons):
+    case = []
+    l = len(dungeons)
+    answer = -1
+
+
+    def make_permutation(depth, dungeons):
+        nonlocal answer
+        if depth == l:
+            case_cnt = 0
+            have = k
+            for need, use in case:
+                if need > have:
+                    break
+                case_cnt += 1
+                have -= use
+            answer = max(answer, case_cnt)
+            return
+        else:
+            for i in range(len(dungeons)):
+                case.append(dungeons[i])
+                make_permutation(depth + 1, dungeons[:i] + dungeons[i + 1:])
+                case.pop()
+    
+    make_permutation(0, dungeons)
+
+    return answer
+
+print(solution(80, [[80,20],[50,40],[30,10]]))
