e3a75ae6ead1a7c10063fba18c99b76f1064d41d
Seok Kyo Oh <osk931@naver.com>
2022-07-25 19:18:31 +0900

    가장 먼 노드

--- Programmers/고득점KIT/Graph/가장 먼 노드.py
+++ Programmers/고득점KIT/Graph/가장 먼 노드.py
@@ -1,43 +1,31 @@
-# BFS 풀이 - 시간초과
 from collections import deque
 
-def solution(n, vertex):
+def solution(n, edge):
     q = deque()
-    map = [
-        [0 for _ in range(n + 1)]
-        for _ in range(n + 1)
-    ]
+    answer = 0
+    graph = [[] for _ in range(n + 1)]
+    distance = [-1 for _ in range(n + 1)]
     
-    dist = [
-        0 for _ in range(n + 1)
-    ]
+    for start, end in edge:
+        graph[start].append(end)
+        graph[end].append(start)
     
-    visited = [
-        False for _ in range(n + 1)
-    ]
-    
-    for a, b in vertex:
-        map[a][b] = map[b][a] = 1
-    
-
-    # 1번 노드 넘버를 큐에 넣는다
+    # 초기설정 - 1번(노드)
     q.append(1)
-    visited[1] = True
+    distance[1] = 0
     
-    curr_max = 0
     while q:
-        prev_node = q.popleft()
-        for next_node in range(1, n + 1):
-            if prev_node != next_node and map[prev_node][next_node] and not visited[next_node]:
+        now = q.popleft()
+        
+        for next_node in graph[now]:
+            if distance[next_node] == -1:
                 q.append(next_node)
-                dist[next_node] = dist[prev_node] + 1
-                if dist[next_node] > curr_max:
-                    curr_max = dist[next_node]
-                    cnt = 1
-                elif dist[next_node] == curr_max:
-                    cnt += 1
-
-                visited[next_node] = True
+                distance[next_node] = distance[now] + 1
     
-    return cnt
-
+    max_value = max(distance)
+    
+    for d in distance:
+        if d == max_value:
+            answer += 1
+                
+    return answer
