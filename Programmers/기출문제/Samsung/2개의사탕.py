1cc5675498addbdb763da3dc6944ed31e4af0463
Seok Kyo Oh <osk931@naver.com>
2022-08-02 17:40:52 +0900

    Solving...

--- Programmers/기출문제/Samsung/2개의사탕.py
+++ Programmers/기출문제/Samsung/2개의사탕.py
@@ -1,6 +1,9 @@
 # Codetree
 # 삼성 SW 역량테스트 2015 하반기 2번
 
+# Variables
+OUT_OF_MAP = (n, m)
+
 tc = int(input())
 
 for _ in range(tc):
@@ -10,3 +13,11 @@ for _ in range(tc):
         list(input())
         for _ in range(n)
     ]
+
+    
+
+# Functions
+
+# 격자에 파란 구슬이 남아있는지 확인
+def blue_exists(blue_pos):
+    return blue_pos != OUT_OF_MAP
