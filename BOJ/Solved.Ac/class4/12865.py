49a950c3fbeb9935a3aa2944447de616a925b69d
Seok Kyo Oh <osk931@naver.com>
2022-08-04 15:24:51 +0900

    12865

--- BOJ/Solved.Ac/class4/12865.py
+++ BOJ/Solved.Ac/class4/12865.py
@@ -73,7 +73,7 @@ dp = [
 for i in range(1, n + 1):
     for j in range(1, k + 1):
         w, v = items[i-1][0], items[i-1][1]
-        if j - w > 0:
+        if j - w >= 0:
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
         else:
             dp[i][j] = dp[i-1][j]
