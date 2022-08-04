c69f9febce8eef529fa588036e1592c994780f97
Seok Kyo Oh <osk931@naver.com>
2022-08-04 15:23:00 +0900

    12865

--- BOJ/Solved.Ac/class4/12865.py
+++ BOJ/Solved.Ac/class4/12865.py
@@ -50,3 +50,33 @@ for i in range(1, N + 1):
 #     print()
 #
 # print(dp2[-1][-1])
+
+
+
+"""
+제출용 코드
+import sys
+input = sys.stdin.readline
+
+n, k = tuple(map(int, input().split()))
+
+items = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+dp = [
+    [0 for _ in range(k + 1)]
+    for _ in range(n + 1)
+]
+
+for i in range(1, n + 1):
+    for j in range(1, k + 1):
+        w, v = items[i-1][0], items[i-1][1]
+        if j - w > 0:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
+        else:
+            dp[i][j] = dp[i-1][j]
+            
+print(dp[n][k])
+"""
