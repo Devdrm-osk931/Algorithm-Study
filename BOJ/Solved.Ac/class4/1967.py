4e3ffe25889f51824f84fbc1767d93545534a381
Seok Kyo Oh <osk931@naver.com>
2022-08-16 21:11:33 +0900

    1967

--- BOJ/Solved.Ac/class4/1967.py
+++ BOJ/Solved.Ac/class4/1967.py
@@ -59,58 +59,56 @@ for node in range(1, n + 1):
 print(ans)
 """
 
-import heapq
+# 위 알고리즘을 개선하여 결국 최대 지점은 Leaf node 중 하나에서 있을 것이라고 생각하여 리프노드에서만 진행했지만 해당 알고리즘도
+# O(n^2logn) 시간복잡도를 갖게 되어 시간초과가 나는 것 같다
+# 트리의 경우 특정 노드를 재방문하여 최단거리가 갱신되는 경우가 발생하지 않기 때문에, dfs/bfs를 사용하여 O(n) 안에 해결이 가능하다.
 
-n = int(input())
+# Solution - O(n)
+# 연결리스트를 이용한 dfs -> O(n)
+import sys
+from collections import deque
 
-# connection
+si = sys.stdin.readline
+
+n = int(si())
+
 graph = [
     []
     for _ in range(n + 1)
 ]
 
 for _ in range(n - 1):
-    a, b, cost = tuple(map(int, input().split()))
+    a, b, cost = tuple(map(int, si().split()))
     graph[a].append((b, cost))
     graph[b].append((a, cost))
 
 
-INF = 1e9
-ans = 0
-def dijkstra(start):
-    dist = [INF] * (n + 1)
-    Q = []
+def bfs(root):
+    max_dist, max_idx = 0, -1
+    dist = [0] * (n + 1)
+    visited = [False] * (n + 1)
+    q = deque()
 
-    # 시작점 설정
-    dist[start] = 0
-    heapq.heappush(Q, (0, start))
+    q.append(root)
+    dist[root] = 0
+    visited[root] = True
 
-    while Q:
-        curr_cost, curr_node = heapq.heappop(Q)
-        for next_node, next_cost in graph[curr_node]:
-            total_cost = curr_cost + next_cost
-            if dist[next_node] < next_cost:
-                continue
-            # 갱신 필요가 있다면 갱신한다
-            if dist[next_node] > total_cost:
-                dist[next_node] = total_cost
-                heapq.heappush(Q, (total_cost, next_node))
-    
-    # 거리가 최대인 점의 인덱스와 거리값을 받는다
-    max_idx = -1
-    max_dist = 0
+    while q:
+        prev_node = q.popleft()
+        for next_node, next_cost in graph[prev_node]:
+            if not visited[next_node]:
+                dist[next_node] = dist[prev_node] + next_cost
+                visited[next_node] = True
+                q.append(next_node)
+
     for i in range(1, n + 1):
-        if dist[i] == INF:
-            continue
-        
-        max_idx = max(max_idx, i)
-        max_dist = max(max_dist, dist[i])
-    
-    return (max_idx, max_dist)
+        if dist[i] > max_dist:
+            max_dist = dist[i]
+            max_idx = i
+
+    return max_idx, max_dist
+
 
 root = 1
-
-# From Root
-idx1, dist1 = dijkstra(root)
-idx2, dist2 = dijkstra(idx1)
-print(dist2)
+f_idx, f_dist = bfs(root)
+print(bfs(f_idx)[1])
